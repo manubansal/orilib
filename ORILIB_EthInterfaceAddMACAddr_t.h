d7264bebbb96e48840de5118be555879113dabc5
Manu Bansal <manub@stanford.edu>
2016-01-26 18:45:19 -0800

    new block to register a mac address on dsp ethernet interface

---
+++ ORILIB_EthInterfaceAddMACAddr_t.h
@@ -0,0 +1,18 @@
+/**
+Atomix project, ORILIB_EthInterfaceAddMACAddr_t.h, TODO: insert summary here
+Copyright (c) 2016 Stanford University
+Released under the Apache License v2.0. See the LICENSE file for details.
+Author(s): Manu Bansal
+*/
+
+#ifndef ORILIB_ETHINTERFACEADDMACADDR_T_H
+#define ORILIB_ETHINTERFACEADDMACADDR_T_H 
+
+#include <osl/inc/swpform.h>
+
+typedef struct {
+  Uint8 mac[6];
+} ORILIB_t_EthInterfaceAddMACAddr;
+
+
+#endif /* ORILIB_ETHINTERFACEADDMACADDR_T_H */
